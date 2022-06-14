853eacc939d1031a103b35ab2596b625666e523b
aadiosap <aadiosap@unal.edu.co>
2022-06-14 05:50:23 -0500

    Intento 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+data = open("pregunta_01\credit.csv", "r").readlines()
+data=[z.split(",")for z in data]
+credit_score=[z[2]for z in data]
+
+
+
+import sys
+
+for valor in credit_score:
+	sys.stdout.write("{}\t1\n".format(valor))
+
+#if __name__ == "__main__":
+#
+#    for line in sys.stdin:
+#        for word in line.split():
+#            sys.stdout.write("{}\t1\n".format(word))
