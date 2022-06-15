b8b3ac56d8363a3228c9544cc10da1dfa7163150
aadiosap <aadiosap@unal.edu.co>
2022-06-15 05:55:26 -0500

    intento3

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+data = open("credit.csv", "r").readlines()
+data=[z.split(",")for z in data]
+purpose_amount=[[z[3],z[4]] for z in data]
+
+
+
+import sys
+
+for key,value in purpose_amount:
+	sys.stdout.write("{}\t{}\n".format(key,value))
