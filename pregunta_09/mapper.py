fb527531f49a65f4be50bf376054d72f1eebcd68
aadiosap <aadiosap@unal.edu.co>
2022-06-27 07:00:36 -0500

    try

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -3,11 +3,11 @@
 #
 data = open("data.csv", "r").readlines()
 data=[z.replace("\n","").split("   ") for z in data]
-pregunta9=[[z[0],z[2][0:4]] for z in data]
+pregunta9=[[z[0],z[1],z[2]] for z in data]
 
 
 
 import sys
 
-for key,value in pregunta9:
-	sys.stdout.write("{}\t{}\n".format(key,value))
+for key,date,value in pregunta9:
+	sys.stdout.write("{}\t{}\t{}\n".format(key,date,value))
--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+contador = 0
+
+if __name__ == '__main__':
+	lista1 = []
+
+	for line in sys.stdin:
+		key,date,val=line.split("\t")
+		val=int(val)
+
+		lista1.append((key,date,val))
+
+		lista1.sort(key=lambda x:(x[2]),reverse=False)
+
+	
+	for elemento in lista1:		
+		sys.stdout.write("{}   {}   {}\n".format(elemento[0],elemento[1],elemento[2]))
+		contador +=1
+		if contador == 6:
+			break
+				
